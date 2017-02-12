54874fd4e3c16aead8dffb15aa2dda01a0998e24
Yeimer Gabriel Molina Ramirez <yeimer.molina@gmail.com>
2017-02-12 11:42:39 -0400

    Add global parameteres in settings

--- shortener/views.py
+++ shortener/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
-
+from django.views import View
 # Create your views here.
+def kirr_redirect_view(request, *args, **kwargs):
+	return HttpResponse("hello")
+
+
+class KirrCBView(View):
+	def get(self, request, *args, **kwargs):
+		return HttpResponse("Hello again")
