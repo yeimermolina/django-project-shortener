4e0ebeec135ec3d0184bf8b1cf76a2cb4597cf63
Yeimer Gabriel Molina Ramirez <yeimer.molina@gmail.com>
2017-02-12 18:38:10 -0400

    Add forms validators

---
+++ shortener/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+
+from .validators import validate_url, validate_dot_com
+
+class SubmitUrlForm(forms.Form):
+	url = forms.CharField(label='submit url', validators=[validate_url, validate_dot_com])
+
+	# def clean(self):
+	# 	cleaned_data = super(SubmitUrlForm, self).clean()
+	# 	print cleaned_data
+	# 	url = cleaned_data['url']
+
+	# def clean_url(self):
+	# 	url = self.cleaned_data['url']
+	# 	url_validator = URLValidator()
+	# 	try:
+	# 		url_validator(url)
+	# 	except:
+	# 		raise forms.ValidationError("Invalid URL for this field")
+	# 	return url
--- shortener/views.py
+++ shortener/views.py
@@ -2,12 +2,45 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.views import View
 
+from .forms import SubmitUrlForm
 from .models import KirrURL
 # Create your views here.
 
 class HomeView(View):
 	def get(self, request, *args, **kwargs):
-		return render(request, "shortener/home.html", {})
+		form = SubmitUrlForm()
+		context = {
+			"title": "SUbmit your url",
+			"form": form
+		}
+		return render(request, "shortener/home.html", context)
+
+	def post(self, request, *args, **kwargs):
+		form = SubmitUrlForm(request.POST)
+
+		context = {
+			"title": "SUbmit your url",
+			"form": form
+		}
+
+		template = "shortener/home.html"
+
+		if form.is_valid():
+			print(form.cleaned_data)
+			new_url = form.cleaned_data.get("url")
+			obj, created = KirrURL.objects.get_or_create(url=new_url)
+			context = {
+				"object": obj,
+				"created": created,
+			}
+
+			if created:
+				template = "shortener/success.html"
+			else:
+				template = "shortener/already-exists.html"
+
+		
+		return render(request, template, context)
 
 
 def kirr_redirect_view(request,  shortcode=None, *args, **kwargs):
