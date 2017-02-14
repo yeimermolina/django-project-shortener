904f5507631d29cce6cfe6eb35626081ba0b8319
Yeimer Gabriel Molina Ramirez <yeimer.molina@gmail.com>
2017-02-14 13:13:16 -0400

    d

--- kirr/settings.py
+++ kirr/settings.py
@@ -62,8 +62,8 @@ ROOT_URLCONF = 'kirr.urls'
 ROOT_HOSTCONF = 'kirr.hosts'
 DEFAULT_HOST = 'www'
 
-DEFAULT_REDIRECT_URL = 'http://www.yeimer-shorten-url.herokuapp.com'
-PARENT_HOST = 'yeimer-shorten-url.herokuapp.com'
+DEFAULT_REDIRECT_URL = 'http://www.yeimer-shorten-url.herokuapp.com:5000'
+PARENT_HOST = 'yeimer-shorten-url.herokuapp.com:5000'
 
 TEMPLATES = [
     {
